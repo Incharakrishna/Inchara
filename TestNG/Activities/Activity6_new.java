f8ad61e64747892efc8ab7b3a3123614366f2822
MalliKGownivari <gownivarimalli@gmail.com>
2025-04-28 12:27:44 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity6_new.java
@@ -0,0 +1,40 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+public class Activity6 {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/login-form");
+    }
+    
+    @Test
+    @Parameters({"username", "password"})
+    public void loginTestCase(String username, String password) {
+        //Find username and pasword fields
+        WebElement usernameField = driver.findElement(By.id("username")).sendKeys(username);
+        WebElement passwordField = driver.findElement(By.id("password")).sendKeys(password);
+
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+        String loginMessage = driver.findElement(By.xpath("//h2")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, Admin!");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
+}
