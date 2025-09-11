ff8f891f4c38bf291a08658cdf85099cf0422db5
Incharakrishna <inchara.arun11@gmail.com>
2025-09-11 09:58:14 +0530

    activity1.java

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,20 @@
+package activities;
+
+public class Activity1 {
+	
+	public static void main(String[] args) {
+		Car Creta = new Car();
+		Creta.make=2014;
+		Creta.color="black";
+		Creta.transmission="manual";
+		
+		Creta.displayCharacteristics();
+		Creta.accelerate();
+		Creta.brake();
+	}
+
+
+
+	}
+
+
